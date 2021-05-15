acdfe03eeb705aaa622f65b061f581ba7765ed73
Bruna Corrêa Cardoso Da Silva <bruna.silva@xpi.com.br>
2021-05-15 02:52:55 -0300

    Details e edit da receita

---
+++ MasterChef.WebApp/Pages/DetailsReceita.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MasterChef.Infrastructure.Data;
+using MasterChef.Infrastructure.Data.EntityConfigurations.API;
+using System.Net.Http;
+
+namespace MasterChef.WebApp.Pages
+{
+    public class DetailsReceitaModel : PageModel
+    {
+        private readonly HttpClient httpClient;
+
+        public DetailsReceitaModel(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public Receita Receita { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            HttpResponseMessage result = await httpClient.GetAsync($"http://localhost:5011/Receita/{id}");
+            result.EnsureSuccessStatusCode();
+
+            Receita = await result.Content.ReadAsAsync<Receita>();
+
+            if (Receita == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ MasterChef.WebApp/Pages/EditReceita.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MasterChef.Infrastructure.Data;
+using MasterChef.Infrastructure.Data.EntityConfigurations.API;
+
+namespace MasterChef.WebApp.Pages
+{
+    public class EditReceitaModel : PageModel
+    {
+        private readonly MasterChef.Infrastructure.Data.ApplicationDbContext _context;
+
+        public EditReceitaModel(MasterChef.Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Receita Receita { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Receita = await _context.Receita
+                .Include(r => r.Categoria).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Receita == null)
+            {
+                return NotFound();
+            }
+           ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Descricao");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Receita).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReceitaExists(Receita.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ReceitaExists(int id)
+        {
+            return _context.Receita.Any(e => e.Id == id);
+        }
+    }
+}
